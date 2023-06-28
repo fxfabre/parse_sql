8e3e483dcd12ecde79f36ef514c28b5a8882a458
Francois-Xavier Fabre <fxfabre@users.noreply.github.com>
2023-06-28 05:53:04 +0200

    Extract joins in queries graph table links + graph fields links between tables

---
+++ scripts/display_field_join.py
@@ -0,0 +1,128 @@
+"""
+Require first to generate file "joins_frequency.csv"
+& grep -v "Function()" joins_frequency.csv > joins_frequency_no_func.csv
+
+Generate a list of all identical fields in different tables
+eg :
+ - pistes.id = opportunity.piste_id = calls.piste_id
+ - table_1.project_id = table_2.project_id
+"""
+import os
+from collections import Counter
+from typing import List
+
+import graphviz
+import pandas as pd
+
+
+def graph_field_joins():
+    df_raw = pd.read_csv(
+        os.path.join(os.getenv("DATA_DIR"), "parse_sql", "joins_frequency.csv"),
+        usecols=["file_name", "left_table", "left_col", "right_table", "right_col"]
+    ).assign(
+        left=lambda df: df["left_table"] + " " + df["left_col"],
+        right=lambda df: df["right_table"] + " " + df["right_col"],
+    )[["file_name", "left", "left_col", "right", "right_col"]]
+
+    df_joins = pd.concat([
+        df_raw.rename(columns={"left": "0", "right": "1"}),
+        df_raw.rename(columns={"right": "0", "left": "1"})
+    ], ignore_index=True)[["0", "1"]].rename(columns={"0": "left", "1": "right"})
+
+    # Save list of clusters, ready to paste into confluence
+    clustered_keys = cluster_same_fields(df_joins)
+    with open("column_joins.txt", "w+") as f:
+        for table_column_names in sorted(clustered_keys, key=len, reverse=True):
+            cols = (col.split(":")[0] for col in table_column_names)
+            lines = [
+                Counter(col for col in cols if col.lower() != "id").most_common(1)[0][0]
+            ]
+            lines.extend(map(format_col_name, sorted(table_column_names)))
+            f.write("### " + "\n> ".join(lines) + "\n\n")
+
+    # Prepare cluster id with file_name, to gen 1 graph per cluster
+    clusters = {
+        table_id: num_cluster
+        for num_cluster, table_ids in enumerate(clustered_keys)
+        for table_id in table_ids
+    }
+
+    df_raw["cluster_id"] = df_raw["left"].map(clusters.get)
+    df_raw.to_csv("clustered.csv", index=False)
+
+    # Generate graphs:
+    for cluster_id, sub_df in df_raw.groupby("cluster_id"):  # .query("cluster_id <= 5")
+        df_cluster = sub_df.groupby(
+            ["left", "left_col", "right", "right_col"],
+            as_index=False,
+        ).agg({"file_name": "\n".join})
+        # pprint(df_cluster)
+
+        table_names = set(df_cluster["left"]) | set(df_cluster["right"])
+        column_names = [
+            col
+            for col in df_cluster["left_col"].tolist() + df_cluster["right_col"].tolist()
+            if col != "id"
+        ]
+
+        if len(table_names) < 5:
+            continue
+        if len(column_names) > 0:
+            cluster_name = Counter(column_names).most_common(1)[0][0]
+        else:
+            cluster_name = df_cluster["left"].iloc[0]
+
+        f = graphviz.Digraph('bigquery', filename=f"cluster_fields/{cluster_name}_{cluster_id}")
+        for node_name in table_names:
+            f.node(node_name)
+        for idx, row in df_cluster.iterrows():
+            print("edge", row["left"], row["right"])
+            f.edge(row["left"], row["right"], row["file_name"])
+        f.view()
+
+
+def cluster_same_fields(df_joins) -> List[set]:
+    dict_joins = df_joins.groupby("left").agg({"right": set}).to_dict()["right"]
+
+    for nb_loop in range(1000):
+        dict_joins = {
+            key: set(values) | {key}
+            for key, values in list(dict_joins.items())
+        }
+
+        counter = Counter(
+            item
+            for k, v in dict_joins.items()
+            for item in v
+        )
+        table_name, count = counter.most_common(1)[0]
+
+        if count <= 1:
+            break
+
+        keys = [
+            key
+            for key, values in dict_joins.items()
+            if table_name in values
+        ]
+
+        all_columns = set()
+        for key in set(keys):
+            # pprint(dict_joins.get(key))
+            all_columns = all_columns | dict_joins.pop(key)
+        dict_joins[table_name] = all_columns
+
+    return list(dict_joins.values())
+
+
+def format_col_name(col_name: str):
+    return col_name
+    dataset = col_name.split(".")[0]
+    col_name = col_name.split(".")[1]
+    table_name = col_name.split(":")[0]
+    field_name = col_name.split(":")[1]
+    return f"{dataset:<30}{table_name:<30}{field_name}"
+
+
+if __name__ == '__main__':
+    graph_field_joins()
---
+++ scripts/extract_joins_from_sql_files.py
@@ -0,0 +1,107 @@
+"""
+First script : read SQL files & generate "joins_frequency.csv"
+"""
+
+import json
+import os
+import sys
+from pathlib import Path
+from pprint import pprint
+
+import pandas as pd
+import traceback
+from parse_sql.extract_from_parsed import extract_all_joins_from_file
+from parse_sql.parsers import read_and_parse_sql_file
+from parse_sql.file_io import get_bq_schema_with_cols
+
+
+def pretty_print(json_content):
+    print(json.dumps(json_content, indent=4))
+
+
+def debug_file():
+    query = "query_groupby.sql"
+    file_path = Path("tests/sql_test_files") / query
+
+    r = read_and_parse_sql_file(file_path)
+    print("\n\n\nResult : ")
+    pretty_print(r)
+
+
+def extract_join_from_all_files(dag_dir: Path):
+    def iter_files(start_folder: Path):
+        if start_folder.is_file():
+            return [start_folder]
+        for root, dirs, files in os.walk(start_folder.as_posix()):
+            root_folder = Path(root).absolute()
+            yield from (
+                root_folder / file_name
+                for file_name in files
+                if file_name.endswith(".sql")
+            )
+
+    bq_schema = get_bq_schema_with_cols()
+
+    join_conditions = []
+    is_parsing_success = []
+    for file_path in iter_files(dag_dir):
+        print(file_path.as_posix())
+        try:
+            parsing_by_cte = read_and_parse_sql_file(file_path)
+            counter_for_file = extract_all_joins_from_file(parsing_by_cte, bq_schema)
+            join_conditions.extend(
+                (join_condition, frequency, file_path.stem)
+                for join_condition, frequency in counter_for_file.items()
+            )
+            is_parsing_success.append(True)
+        except Exception as e:
+            print("  Failed :", e)
+            traceback.print_tb(e.__traceback__)
+            is_parsing_success.append(False)
+
+    df_join_conditions = pd.DataFrame(
+        join_conditions, columns=["join_condition", "frequency", "file_name"]
+    )
+
+    print(f"parsing success : {sum(is_parsing_success)} / {len(is_parsing_success)}")
+    pprint(
+        df_join_conditions[["join_condition", "frequency"]]
+        .groupby("join_condition", as_index=False)
+        .agg({"frequency": sum})
+        .sort_values("frequency", ascending=False)
+        .head(30)
+        .to_dict("records")
+    )
+    save_file_joins_frequency(df_join_conditions)
+
+
+def save_file_joins_frequency(df):
+    csv_file_path = os.path.join("joins_frequency")  # os.getenv("DATA_DIR"), "parse_sql"
+    # os.makedirs(os.path.dirname(csv_file_path), exist_ok=True)
+    df.sort_values("frequency", ascending=False).to_csv(csv_file_path + "_raw.csv", index=False)
+
+    df = df.assign(
+        join_condition=lambda _df: _df["join_condition"].str.lower(),
+
+        left=lambda _df: _df["join_condition"].map(lambda join: join.split("=")[0].strip().lower()),
+        left_table=lambda _df: _df["left"].map(lambda x: x.split(":")[0] if ":" in x else ""),
+        left_col=lambda _df: _df["left"].map(lambda x: x.split(":")[1] if ":" in x else x),
+
+        right=lambda _df: _df["join_condition"].map(lambda join: join.split("=")[1].strip().lower()),
+        right_table=lambda _df: _df["right"].map(lambda x: x.split(":")[0] if ":" in x else ""),
+        right_col=lambda _df: _df["right"].map(lambda x: x.split(":")[1] if ":" in x else x),
+    ).where(
+        # rm tmp tables
+        lambda _df: _df["left_table"].str.contains(".", regex=False) &
+                    _df["right_table"].str.contains(".", regex=False)
+    )[["file_name", "left_table", "left_col", "right_table", "right_col", "frequency"]].dropna()
+
+    df.sort_values("frequency", ascending=False).to_csv(csv_file_path + ".csv", index=False)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        dag_dir = Path(sys.argv[1])
+    else:
+        dag_dir = Path(os.getenv("WORK_DIR")) / "data-flow" / "dags"
+    extract_join_from_all_files(dag_dir)
